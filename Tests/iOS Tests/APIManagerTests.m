7e7f0a9ea7663bf241bf522afc825de6b117e8dc
Dmitriy Utmanov <utm4@mail.ru>
2015-03-09 18:58:46 +0300

    Test fix

---
+++ Tests/iOS Tests/APIManagerTests.m
@@ -0,0 +1,41 @@
+//
+//  iOS_Test_Tests.m
+//  iOS Test Tests
+//
+//  Created by Dmitriy Utmanov on 09/03/15.
+//  Copyright (c) 2015 Dmitriy Utmanov. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import "DCAPIManager.h"
+
+NSString * const DCClassTestsBaseURLString = @"https://httpbin.org/";
+
+
+@interface APIManagerTests : XCTestCase
+
+@end
+
+
+@implementation APIManagerTests
+
+- (void)setUp {
+  [super setUp];
+  // Put setup code here. This method is called before the invocation of each test method in the class.
+  [DCAPIManager setSharedManagerWithBaseURL:[NSURL URLWithString:DCClassTestsBaseURLString]];
+}
+
+- (void)tearDown {
+  [[DCAPIManager sharedManager] invalidateSessionCancelingTasks:YES];
+  // Put teardown code here. This method is called after the invocation of each test method in the class.
+  [super tearDown];
+}
+
+- (void)testConnect {
+    XCTAssert(YES, @"Pass");
+}
+
+@end
